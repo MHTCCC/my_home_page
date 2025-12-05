f18c28b65466615ef892ab451226966499ac2830
MHT <498711269@qq.com>
2025-12-06 00:30:17 +0800

    modify vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/my_home_page/",
     plugins: [react(), tailwindcss()],
     server: {
         host: '0.0.0.0',
